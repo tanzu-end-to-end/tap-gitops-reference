683f22c4eea98f414053e399579b15b388ec917e
Jeff <11721615+jeffellin@users.noreply.github.com>
2022-07-24 19:50:26 -0400

    maybe

--- scripts/create-kubeconfig.sh
+++ scripts/create-kubeconfig.sh
@@ -15,12 +15,13 @@ ADJUSTED_CLUSTER_NAME=${CLUSTER_NAME#*@*}
 ADJUSTED_CLUSTER_NAME=${ADJUSTED_CLUSTER_NAME//-/_}
 
 export API_SERVER_URL=$(kubectl config view --minify --flatten -o jsonpath='{.clusters[0].cluster.server}')
-yq e -i '.clusters.view.tap_gui.'$ADJUSTED_CLUSTER_NAME'.api_server_url = env(API_SERVER_URL)' $PARAMS_YAML
+yq e -i '.clusters.[0].cluster.server = env(API_SERVER_URL)' kubeconfig.yaml
 
 export API_SERVER_CA=$(kubectl config view --minify --flatten -o jsonpath='{.clusters[0].cluster.certificate-authority-data}')
-yq e -i '.clusters.view.tap_gui.'$ADJUSTED_CLUSTER_NAME'.api_server_ca = env(API_SERVER_CA)' $PARAMS_YAML
-
+yq e -i '.clusters[0].cluster.certificate-authority-data = env(API_SERVER_CA)' kubeconfig.yaml
+#
 export SA_TOKEN=$(kubectl -n tap-gui get secret $(kubectl -n tap-gui get sa  tap-gui-viewer-tapview -o=json | jq -r '.secrets[0].name') -o=json | jq -r '.data["token"]' | base64 --decode)
-yq e -i '.clusters.view.tap_gui.'$ADJUSTED_CLUSTER_NAME'.sa_token = env(SA_TOKEN)' $PARAMS_YAML
+echo $SA_TOKEN
+yq e -i '.users[0].user.token = env(SA_TOKEN)' kubeconfig.yaml
 
-echo "View cluster config updated, now you have to apply updates"
+#echo "View cluster config updated, now you have to apply updates"
