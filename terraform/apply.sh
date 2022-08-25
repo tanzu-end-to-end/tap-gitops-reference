517d7970df0a8b8fb3215411c0f7e5e47b6177fb
Jeff <11721615+jeffellin@users.noreply.github.com>
2022-08-25 14:50:30 -0400

    cleanup before 1.3 bug bash

--- terraform/apply.sh
+++ terraform/apply.sh
@@ -5,7 +5,7 @@ terraform fmt
 terraform apply -auto-approve
 
 # Retrieve the list of output commands from terraform output.  This is map of cluster name as keys with command as value
-APPLY_TMC_CMDS=$(terraform output -json tmc_execution_commands)
+APPLY_TMC_CMDS=$(cat clusters.json | jq -c .clusters)
 
 # Loop over the cluster names in the output
 for cluster in $(echo $APPLY_TMC_CMDS | jq "keys" | jq -r 'join(" ")')
@@ -14,18 +14,18 @@ do
   echo "Processing $cluster cluster"
 
   # Retrieve the kubecontext for the cluster
-  aws eks update-kubeconfig --region us-east-2 --name $cluster
+  ##aws eks update-kubeconfig --region us-east-2 --name $cluster
 
   # Attach the cluster to TMC
-  APPLY_TMC_CMD=$(echo $APPLY_TMC_CMDS | jq -r '."'$cluster'"')
+ ## APPLY_TMC_CMD=$(echo $APPLY_TMC_CMDS | jq -r '."'$cluster'"')
 
-  echo "$cluster apply command: $APPLY_TMC_CMD"
+##  echo "$cluster apply command: $APPLY_TMC_CMD"
 
-  if [ ! -z "$APPLY_TMC_CMD" ] && [ "$APPLY_TMC_CMD" != "missing" ]
-  then
+##  if [ ! -z "$APPLY_TMC_CMD" ] && [ "$APPLY_TMC_CMD" != "missing" ]
+ ## then
 
-    eval "$APPLY_TMC_CMD"
+##    eval "$APPLY_TMC_CMD"
 
-  fi
+##  fi
 
 done
--- terraform/outputs.tf
+++
@@ -1,6 +0,0 @@
-output "tmc_execution_commands" {
-  value = {
-    # the execution_cmd is only present in the output in the beginning.  So must test for existance
-    for k, v in tanzu-mission-control_cluster.tap_cluster : k => lookup(v.status, "execution_cmd", "missing")
-  }
-}
