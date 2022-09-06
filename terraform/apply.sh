c75c974ab83abc3f6cf932e367971f33f7702803
Jeff <11721615+jeffellin@users.noreply.github.com>
2022-09-06 12:25:40 -0400

    update to 1.3.0.build.18

--- terraform/apply.sh
+++ terraform/apply.sh
@@ -1,22 +1,37 @@
 #!/bin/bash -ex
+start=`date +%s`
 
 # Retrieve the list of output commands from terraform output.  This is map of cluster name as keys with command as value
 #APPLY_TMC_CMDS=$(cat clusters.json | jq -c .clusters)
+: ${PARAMS_YAML?"Need to set PARAMS_YAML environment variable"}
 
-for im in $(yq eval -o=j servers.yaml | jq -cr '.servers[]'); do
+
+# remote repo for install cluster essentials.
+export INSTALL_BUNDLE=registry.tanzu.vmware.com/tanzu-cluster-essentials/cluster-essentials-bundle@sha256:e00f33b92d418f49b1af79f42cb13d6765f1c8c731f4528dfff8343af042dc3e
+export INSTALL_REGISTRY_HOSTNAME=registry.tanzu.vmware.com
+export INSTALL_REGISTRY_USERNAME=$(yq eval '.tap.tanzunet.username' $PARAMS_YAML)
+export INSTALL_REGISTRY_PASSWORD=$(yq eval '.tap.tanzunet.password' $PARAMS_YAML)
+
+for im in $(yq e -I=0 -o=j '.servers[]' servers.yaml); do
+     
       cluster=$(echo $im | jq -r '.name' -)
-      # eksctl create cluster --config-file eks-${cluster}.yaml
-      #aws eks update-kubeconfig --region us-east-2 --name $cluster
+      config=$(echo $im | jq -r '.config' -)
+      region=$(echo $im | jq -r '.region' -)
+      eksctl create cluster --config-file ${config} 
+      aws eks update-kubeconfig --region ${region} --name $cluster
       # add cluster essentials
-      export INSTALL_BUNDLE=registry.tanzu.vmware.com/tanzu-cluster-essentials/cluster-essentials-bundle@sha256:e00f33b92d418f49b1af79f42cb13d6765f1c8c731f4528dfff8343af042dc3e
-      export INSTALL_REGISTRY_HOSTNAME=registry.tanzu.vmware.com
-      export INSTALL_REGISTRY_USERNAME=jellin@pivotal.io
-      export INSTALL_REGISTRY_PASSWORD=<wrong>
-      cd $HOME/tanzu-cluster-essentials
+      pushd $HOME/tanzu-cluster-essentials
               ./install.sh --yes
       # add repos for tanzu standard
-      kubectl apply -f ../../tanzu-standard.yaml --dry-run=client
+      popd 
+
+      kubectl apply -f tanzu-standard.yaml
+      #allow packages to reconcile
+      sleep 30
       # add metrics
       tanzu package install metrics-server --package-name metrics-server.tanzu.vmware.com --version 0.5.1+vmware.1-tkg.1
 done
+end=`date +%s`
+
+runtime=$((end-start))
 
---
+++ terraform/destroy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -ex
+for im in $(yq e -I=0 -o=j '.servers[]' servers.yaml); do
+      cluster=$(echo $im | jq -r '.name' -)
+      config=$(echo $im | jq -r '.config' -)
+      region=$(echo $im | jq -r '.region' -)
+      eksctl delete cluster  --region=${region} --name ${cluster}  --disable-nodegroup-eviction --pod-eviction-wait-period 1s   
+done
+
