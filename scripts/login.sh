646bd3040ccb729b9b835cb8ceb43ae19a8aa41b
Jeff <11721615+jeffellin@users.noreply.github.com>
2022-10-17 09:27:01 -0400

    update login script

--- scripts/login.sh
+++ scripts/login.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
 : ${PARAMS_YAML?"Need to set PARAMS_YAML environment variable"}
 
@@ -7,13 +7,11 @@ INFRA_LAB_CONFIG_YAML=local-config/infra-lab-values.yaml
 export VSPHERE_WITH_TANZU_PASSWORD=$(yq e .h2o.password $PARAMS_YAML)
 
 # if first argument is "admin" then use the H2O login info
-#if [ "$1" = "admin" ]; then
-#    export USER=$(yq e .vsphere.admin_username $INFRA_LAB_CONFIG_YAML)
-#    export KUBECTL_VSPHERE_PASSWORD=$(yq e .vsphere.admin_password $INFRA_LAB_CONFIG_YAML)
-#else
-#    export USER=$1@vsphere.local
-#    export KUBECTL_VSPHERE_PASSWORD=$(yq e .vsphere.standard_user_password $INFRA_LAB_CONFIG_YAML)
-#fi
+if [ "$1" = "admin" ]; then
+    export USER=$(yq e .h2o.admin_username $PARAMS_YAML)
+else
+    export USER=$1@vsphere.local
+fi
 
 export SUPERVISOR_CLUSTER=$(yq e .h2o.supervisor.host $PARAMS_YAML)
 
@@ -26,8 +24,6 @@ if [ -z "$2" ]; then
     send -- \"$VSPHERE_WITH_TANZU_PASSWORD\r\"
     expect eof
     ")
-
-    #kubectl vsphere login --server $SUPERVISOR_CLUSTER -u $USER --insecure-skip-tls-verify
 
     kubectl config use-context $SUPERVISOR_CLUSTER
 
@@ -48,14 +44,12 @@ elif [ "$2" = "all" ]; then
 else
     export TKC_NAME=$3
     export VSPHERE_NAMESPACE_NAME=$2
-#    KUBECTL_VSPHERE_LOGIN_COMMAND=$(expect -c "
-#    spawn kubectl vsphere  login --server=$SUPERVISOR_CLUSTER -u $1 --tanzu-kubernetes-cluster-name $TKC_NAME --tanzu-kubernetes-cluster-namespace $VSPHERE_NAMESPACE_NAME --insecure-skip-tls-verify
-#    expect \"*?assword:*\"
-#    send -- \"$VSPHERE_WITH_TANZU_PASSWORD\r\"
-#    expect eof
-#    ")
-    kubectl vsphere login --server $SUPERVISOR_CLUSTER -u $USER --tanzu-kubernetes-cluster-name $TKC_NAME --tanzu-kubernetes-cluster-namespace $VSPHERE_NAMESPACE_NAME --insecure-skip-tls-verify
-
+    KUBECTL_VSPHERE_LOGIN_COMMAND=$(expect -c "
+    spawn kubectl vsphere  login --server $SUPERVISOR_CLUSTER -u $USER --tanzu-kubernetes-cluster-name $TKC_NAME --tanzu-kubernetes-cluster-namespace $VSPHERE_NAMESPACE_NAME --insecure-skip-tls-verify
+    expect \"*?assword:*\"
+    send -- \"$VSPHERE_WITH_TANZU_PASSWORD\r\"
+    expect eof
+    ")
     kubectl config use-context $TKC_NAME
 
 fi
